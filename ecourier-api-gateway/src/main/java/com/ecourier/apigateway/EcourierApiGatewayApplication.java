bd3aa1e2f7a06cdb9bf06de4673d49aaa59dd3ce
Atekur Rahman <atekur.rm@gmail.com>
2025-06-29 21:53:31 +0600

    Add initial API Gateway setup with Maven wrapper, application properties, and Eureka client configuration

---
+++ ecourier-api-gateway/src/main/java/com/ecourier/apigateway/EcourierApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.ecourier.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EcourierApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcourierApiGatewayApplication.class, args);
+	}
+
+}
